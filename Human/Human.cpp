d2a46b2d38f9c7de75b6a04a06f16737038590ea
njrfvnjkn <test@gmail.com>
2025-09-08 13:17:42 +0300

    ""

--- Human/Human.cpp
+++ Human/Human.cpp
@@ -1,1 +1,48 @@
-#include "Human.h"
+#include "Human.h" // файл реализации
+
+Human::Human()
+{
+}
+
+Human::Human(const char* n, const char* s, int a)
+{
+}
+
+Human::~Human()
+{
+}
+
+void Human::Input()
+{
+}
+
+void Human::Print()
+{
+}
+
+char* Human::GetName()
+{
+	return nullptr;
+}
+
+char* Human::GetSurname()
+{
+	return nullptr;
+}
+
+int Human::GetAge()
+{
+	return 0;
+}
+
+void Human::SetName(const char* n)
+{
+}
+
+void Human::SetSurname(const char* s)
+{
+}
+
+void Human::SetAge(int a)
+{
+}
--- Human/Human.h
+++ Human/Human.h
@@ -1,6 +1,24 @@
-#pragma once
+#pragma once // файл дизайна
 class Human
 {
+	char* name;
+	char* surname;
+	int age;
+
+public:
+	Human();
+	Human(const char* n, const char* s, int a);
+	~Human();
+	void Input();
+	void Print();
+
+	char* GetName();
+	char* GetSurname();
+	int GetAge();
+
+	void SetName(const  char * n);
+	void SetSurname(const  char* s);
+	void SetAge(int a);
 
 };
 
